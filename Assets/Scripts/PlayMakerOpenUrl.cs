8ec89eb3e0569de21bc6f41ed529fad60f3bc6a2
DESKTOP-FVSCF23\htw75 <htw750128@gmail.com>
2019-12-06 14:06:42 +0800

    第一版上去

---
+++ Assets/Scripts/PlayMakerOpenUrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Device)]
+	[Tooltip("打開網頁")]
+	public class PlayMakerOpenUrl : FsmStateAction
+	{
+		
+		[RequiredField]
+		[UIHint(UIHint.TextArea)]
+		public FsmString Url;
+		
+
+	
+		public override void OnEnter()
+		{
+		Application.OpenURL(Url.Value);
+		Finish();
+		}
+		
+
+	}
+}
---
+++ Assets/Scripts/SwitchingtoVRmodeatruntime.cs
@@ -0,0 +1,23 @@
+﻿// Run in split-screen mode
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class SwitchingtoVRmodeatruntime : MonoBehaviour
+{
+   /* void Start()
+    {
+        StartCoroutine(LoadDevice("cardboard"));
+    }*/
+
+    IEnumerator LoadDevice(string newDevice)
+    {
+        if (String.Compare(XRSettings.loadedDeviceName, newDevice, true) != 0)
+        {
+            XRSettings.LoadDeviceByName(newDevice);
+            yield return null;
+            XRSettings.enabled = true;
+        }
+    }
+}
